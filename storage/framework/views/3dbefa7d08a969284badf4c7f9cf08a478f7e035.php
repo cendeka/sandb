3b041eeb968a04498cd83e4da8721f34f22d255d
ilhamtaufiq <ilhamtaufiq97@gmail.com>
2022-11-05 18:56:56 +0700

    remove workflow

--- storage/framework/views/3dbefa7d08a969284badf4c7f9cf08a478f7e035.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-	  <div class="container-fluid">
-		    <div class="row">
-			      <div class="col-md-12 footer-copyright text-center">
-			        	<p class="mb-0">Copyright <?php echo e(date('Y')); ?> © ilhamtopiq  </p>
-			      </div>
-		    </div>
-	  </div>
-</footer><?php /**PATH /var/www/sandb/resources/views/layouts/simple/footer.blade.php ENDPATH**/ ?>
